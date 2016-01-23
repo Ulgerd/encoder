a94cb25af0ffffb189fc21a2c20e47c6bc94967b
DitRIX <melky@li.ru>
2016-01-24 00:14:49 +0300

    initial version

---
+++ encoder.rb
@@ -0,0 +1,26 @@
+#coding: utf-8
+
+require 'digest'
+
+puts "Введите слово или фразу для шифрования:"
+frase = STDIN.gets
+
+puts "Каким способом зашифровать:"
+puts "1. MD5"
+puts "2. SHA1"
+encoder = STDIN.gets.to_i
+
+if encoder == 1
+  md5 = Digest::MD5.new
+  md5 << frase
+  puts "Вот что получилось:"
+  puts md5.hexdigest
+elsif encoder == 2
+  sha1 = Digest::SHA1.new
+  puts "Вот что получилось:"
+  puts sha1.digest 'frase'
+else
+  puts "Вы ошиблись!"
+end
+
+
