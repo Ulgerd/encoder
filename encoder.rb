8585169c06cede658d1fb9d0241f939125f22308
DitRIX <melky@li.ru>
2016-01-24 12:33:01 +0300

    sha2

--- encoder.rb
+++ encoder.rb
@@ -8,6 +8,7 @@ frase = STDIN.gets
 puts "Каким способом зашифровать:"
 puts "1. MD5"
 puts "2. SHA1"
+puts "3. SHA2"
 encoder = STDIN.gets.to_i
 
 if encoder == 1
@@ -19,6 +20,10 @@ elsif encoder == 2
   sha1 = Digest::SHA1.new
   puts "Вот что получилось:"
   puts sha1.digest 'frase'
+elsif encoder == 3
+  sha2 = Digest::SHA2.new
+  puts "Вот что получилось:"
+  puts sha2.digest 'frase'
 else
   puts "Вы ошиблись!"
 end
